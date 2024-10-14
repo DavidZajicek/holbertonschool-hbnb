3a48f649dd26e0085d7eb9cfcf73200cc4b6f5da
chutima puthachon <chutima.p8@gmail.com>
2024-10-15 08:49:27 +1100

    added basemodel

---
+++ app/models/amenities.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# from __init__ import BaseModel
+# from app.models.__init__ import BaseModel
+
+# class Amenity(BaseModel):
+#     def __init__(self, name):
+#         super().__init__()
+#         self.name = name
+
+#     def to_dict(self):
+#         amenity_dict = super().to_dict()
+#         amenity_dict.update({
+#             'name': self.name,
+#         })
+#         return amenity_dict
