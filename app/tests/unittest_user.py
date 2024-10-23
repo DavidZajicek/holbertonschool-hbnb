e8a1ce6af822dec78b84d1fc8ce53e109b8d5136
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-10-23 14:34:21 +1100

    Unittest user

--- app/tests/unittest_user.py
+++ app/tests/unittest_user.py
@@ -1,12 +1,11 @@
 import unittest
-# from app import create_app
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from app import create_app
 
 class TestUserEndpoints(unittest.TestCase):
 
-    # def test_function(self):
-    #     print ('hello')
-    #     self.assertEqual('a', 'b')
 
     def setUp(self):
         self.app = create_app()
@@ -20,52 +19,53 @@ class TestUserEndpoints(unittest.TestCase):
             "email": "jane.doe@example.com"
         })
         self.assertEqual(response.status_code, 201)
-        self.assertIn('User created successfully', response.get_json()['message'])
-    
-    # def test_create_user_existing_email(self):
-    #     #create the user first
-    #     self.client.post('/api/v1/users/', json={
-    #         "first_name": "Amy",
-    #         "last_name": "Beford",
-    #         "email": "amy@example.com"
-    #     })
-    #     #test 
-    #     response = self.client.post('/api/v1/users', json={
-    #         "first_name": "Jane",
-    #         "last_name": "Doe",
-    #         "email": "amy@example.com"
-    #     })
-    #     self.asserEqual(response.status_code, 400)
-    #     self.assertIn("Email already registered", response.get_json()['Error'])
+        self.assertIn('jane.doe@example.com', response.get_json()['email'])
 
-    # def test_create_user_invalid_data(self):
-    #     response = self.client.post('/api/v1/users/', json={
-    #         "first_name": "",
-    #         "last_name": "",
-    #         "email": "invalid-email"
-    #     })
-    #     self.assertEqual(response.status_code, 400)
-    #     self.assertIn('Invalid input data', response.get_json()['error'])
+    def test_create_user_existing_email(self):
+    #create the user first
+        self.client.post('/api/v1/users/', json={
+            "first_name": "Amy",
+            "last_name": "Beford",
+            "email": "amy@example.com"
+        })
+    #test
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "amy@example.com"
+        })
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Email already registered', response.get_json())
 
-    # def test_update_user(self):
-    #     #Create the user first
-    #     response = self.client.post('/api/v1/users/', json={
-    #         "first_name": "Amy",
-    #         "last_name": "Smith",
-    #         "email": "amy@example.com"
+    def test_create_user_invalid_data(self):
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "Amy",
+            "last_name": "Smith",
+            "email": "invalid-email"
+            })
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Invalid input data', response.get_json())
 
-    #     user_id = user_response.get_json()['id']
+    def test_update_user(self):
+        #Create the user first
+        user_response = self.client.post('/api/v1/users/', json={
+            "first_name": "Amy",
+            "last_name": "Smith",
+            "email": "amy@example.com"
+        })
 
-    #     response = self.client.put(f'/api/v1/users/{user_id}', json= {
-    #         "first_name": "Amy",
-    #         "last_name": "Doe",
-    #         "email": "amy.doe@example.com"
-    #     })
+        user_id = user_response.get_json()['id']
 
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.get_json()['first_name'], "Amy")
-    #     self.assertEqual(response.get_json()['first_name'], "Doe")
-    #     self.assertEqual(response.get_json()['first_name'], "amy.doe@example.com")
+        response = self.client.put(f'/api/v1/users/{user_id}', json= {
+            "first_name": "Amy",
+            "last_name": "Doe",
+            "email": "amy.doe@example.com"
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json()['first_name'], "Amy")
+        self.assertEqual(response.get_json()['last_name'], "Doe")
+        self.assertEqual(response.get_json()['email'], "amy.doe@example.com")
 
 if __name__ == '__main__':
     unittest.main()
