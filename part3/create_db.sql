17c5d406b2ea421acd2c58bd3a2785e48f88778b
David Zajicek <david.a.zajicek@outlook.com>
2024-11-05 19:20:00 +1100

    sql creation and tests

--- part3/create_db.sql
+++ part3/create_db.sql
@@ -1,66 +1,90 @@
--- create user
-CREATE USER IF NOT EXISTS 'hbnb_evo_2'@'localhost' IDENTIFIED BY 'hbnb_evo_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'hbnb_evo_2'@'localhost';
-
 -- Create and use the specified database
 CREATE DATABASE IF NOT EXISTS hbnb_evo_2_db;
 USE hbnb_evo_2_db;
+DROP TABLE IF EXISTS `place_amenity`;
+DROP TABLE IF EXISTS `reviews`;
+DROP TABLE IF EXISTS `amenities`;
+DROP TABLE IF EXISTS `places`;
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `id` varchar(60) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(120) NOT NULL UNIQUE,
-  `password` varchar(128) NOT NULL,
-  `is_admin` bool DEFAULT FALSE,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-DROP TABLE IF EXISTS `places`;
+    id CHAR(36) PRIMARY KEY NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOL DEFAULT FALSE
+);
 CREATE TABLE `places` (
-  `id` varchar(60) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `title` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `price` float NOT NULL,
-  `latitude` float NOT NULL,
-  `longitude` float NOT NULL,
-  `owner_id` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `owner_id` (`owner_id`),
-  CONSTRAINT `places_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-DROP TABLE IF EXISTS `reviews`;
+    id CHAR(36) PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL CHECK (price > 0),
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    owner_id CHAR(36) NOT NULL, 
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+);
 CREATE TABLE `reviews` (
-  `id` varchar(60) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `text` text NOT NULL,
-  `rating` integer NOT NULL,
-  `place_id` varchar(60) NOT NULL,
-  `user_id` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `place_id` (`place_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`),
-  CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-DROP TABLE IF EXISTS `amenities`;
+    id CHAR(36) PRIMARY KEY NOT NULL,
+    `text` TEXT NOT NULL,
+    rating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    user_id CHAR(36) NOT NULL,
+    place_id CHAR(36) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    CONSTRAINT UC UNIQUE (user_id, place_id)
+);
 CREATE TABLE `amenities` (
-  `id` varchar(60) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-DROP TABLE IF EXISTS `place_amenity`;
+    id CHAR(36) PRIMARY KEY NOT NULL,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
 CREATE TABLE `place_amenity` (
-  `place_id` varchar(60) NOT NULL,
-  `amenity_id` varchar(60) NOT NULL,
-  PRIMARY KEY (`place_id`,`amenity_id`),
-  KEY `amenity_id` (`amenity_id`),
-  CONSTRAINT `place_amenity_ibfk_1` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`),
-  CONSTRAINT `place_amenity_ibfk_2` FOREIGN KEY (`amenity_id`) REFERENCES `amenities` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    place_id CHAR(36) NOT NULL,
+    amenity_id CHAR(36) NOT NULL,
+    PRIMARY KEY (place_id, amenity_id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
+);
+
+
+-- Insert Initial Data into the tables
+INSERT INTO users (id, email, first_name, last_name, password, is_admin)
+VALUES (
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+    'admin@hbnb.io',
+    'Admin',
+    'HBnB',
+    '5319c237fd4e9e3896ebac929552c4e8',
+    1
+),
+(
+    '46c9050e-ddd3-4c3b-9731-9f487208bbc1',
+    'notmin@hbnb.ii',
+    'Notmin',
+    'HBnB',
+    '5319c237fd4e9e3896ebac929552c4e8',
+    0
+);
+INSERT INTO amenities (id, name)
+VALUES 
+("a0d0ccf7-ee4e-4a33-879b-34a16d398c1b", "WiFi"),
+("b0d0ccf7-ee4e-4a33-879b-34a16d398c1b", "Swimming Pool"),
+("c0d0ccf7-ee4e-4a33-879b-34a16d398c1b", "Air Conditioning");
+SELECT * FROM amenities;
+
+-- Delete a value
+DELETE FROM amenities WHERE name="WiFi";
+SELECT * FROM amenities;
+
+-- Try to update a user's email to one that's already in (unique constraint test)
+-- Uncomment to test dupes
+-- UPDATE users SET email = "admin@hbnb.io" WHERE id = '46c9050e-ddd3-4c3b-9731-9f487208bbc1';
+SELECT * FROM users;
+
+-- update a value to a value that was previously taken to test integrity
+UPDATE amenities
+SET id = "a0d0ccf7-ee4e-4a33-879b-34a16d398c1b"
+WHERE id = "b0d0ccf7-ee4e-4a33-879b-34a16d398c1b";
+
+
+SELECT * FROM amenities;
